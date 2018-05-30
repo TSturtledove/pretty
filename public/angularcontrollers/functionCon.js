530e7f6d0707d7fd032b985814ff4dcd3efc4fa4
TSturtledove <roberttortorelli@yahoo.com>
2018-05-30 17:09:39 -0500

    trying to get a function button to work

--- public/angularcontrollers/functionCon.js
+++ public/angularcontrollers/functionCon.js
@@ -1,7 +1,21 @@
 app.controller("functionCon", function($scope, $route) {
-
+  let count = 1
+  console.log("hi")
   $scope.jam = "cinzelsmall"
   $scope.jelly = "bitter"
   $scope.butter ="droids"
+  $scope.number = count
+  const popPage = function() {
+    $scope.number = count
+    $scope.$apply()
+  }
+
+  $scope.trythis = function() {
+    console.log("yo")
+    // if(count>=0 && count!=2){
+    //   count = 2
+    // }else{count = 4}
+    // popPage()
+  }
 
 })
