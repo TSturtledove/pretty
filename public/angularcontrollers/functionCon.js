9f8200831cb0804e11c06b4d2da2da29dfbda886
TSturtledove <roberttortorelli@yahoo.com>
2018-06-02 22:19:47 -0500

    trying to figure out why the function isn't working

--- public/angularcontrollers/functionCon.js
+++ public/angularcontrollers/functionCon.js
@@ -7,15 +7,26 @@ app.controller("functionCon", function($scope, $route) {
   $scope.number = count
   const popPage = function() {
     $scope.number = count
+    console.log("tellme")
     $scope.$apply()
   }
 
-  $scope.trythis = function() {
+  $scope.trythis = () =>{
     console.log("yo")
     // if(count>=0 && count!=2){
     //   count = 2
     // }else{count = 4}
-    // popPage()
+    popPage()
   }
 
+  $scope.Profile = () => {
+    console.log("you hit the profile button")
+  //   authFactory.gatecheck()
+  //   .then( () => {
+  //     $scope.$apply(function() { $route.path("/profile") })
+  //     console.log("got back from gatecheck")
+  //   }).catch( (err)=> {
+  //     console.log("got an error at gatecheck", err)
+  //   })
+  }
 })
